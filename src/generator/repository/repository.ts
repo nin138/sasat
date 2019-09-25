8ac8e8c965fd587db727b44ccbbe6f11838f44c9
yanokunpei <ninian138@gmail.com>
2019-09-25 17:12:02 +0900

    fix gen repo

--- src/generator/repository/repository.ts
+++ src/generator/repository/repository.ts
@@ -7,13 +7,9 @@ import { getEntityName } from '../entity/entity';
 import { columnTypeToTsType } from '../../migration/column/columnTypes';
 import { ReferenceColumnInfo } from '../../migration/column/referenceColumn';
 
-const importStatement = (table: TableInfo) =>
-  [
-    `import { SasatRepository } from 'sasat';`,
-    `import { ${getEntityName(table)}, Creatable${getEntityName(table)} } from '../entity/${table.tableName}';`,
-  ].join('\n');
+// TODO refactoring
 
-const createRepositoryFindFn = (
+const createFindFunction = (
   name: string,
   params: string[],
   returns: string,
@@ -28,69 +24,87 @@ const createRepositoryFindFn = (
   }
 `;
 
-const findBy = (
-  table: TableInfo,
-  keys: string[],
-  unique: boolean,
-  ref?: Pick<ReferenceColumnInfo, 'table' | 'column'>,
-) => {
-  if (keys.length === 0) return '';
-  const name = ref !== undefined ? ref!.table : keys.map(capitalizeFirstLetter).join('And');
-  const params =
-    ref !== undefined
-      ? [`${ref.table}: Pick<${capitalizeFirstLetter(ref!.table)}, '${ref.column}'>`]
-      : keys.map(it => `${it}: ${columnTypeToTsType(table.columns.find(it => it.columnName)!.type)}`);
-  const returns = unique ? `${getEntityName(table)} | undefined` : `${getEntityName(table)}[]`;
-  const findParamMap = ref !== undefined ? [`${ref.column}: ${ref.table}.${ref.column}`] : keys;
-  return createRepositoryFindFn(name, params, returns, findParamMap, unique);
-};
+export class RepositoryGenerator {
+  private imports: string[] = ["import { SasatRepository } from 'sasat';"];
+  constructor(private table: TableInfo) {
+    this.imports.push(
+      `import { ${getEntityName(table)}, Creatable${getEntityName(table)} } from '../entity/${table.tableName}';`,
+    );
+  }
 
-const functions = (table: TableInfo) => {
-  const functions: Array<{ keys: string[]; unique: boolean; ref?: Pick<ReferenceColumnInfo, 'table' | 'column'> }> = [];
-
-  const isDuplicate = (keys: string[]) => {
-    outer: for (const fn of functions) {
-      if (keys.length !== fn.keys.length) continue;
-      for (const [i, it] of keys.entries()) {
-        if (it !== fn.keys[i]) continue outer;
-      }
-      return true;
+  private createFindBy = (
+    table: TableInfo,
+    keys: string[],
+    unique: boolean,
+    ref?: Pick<ReferenceColumnInfo, 'table' | 'column'>,
+  ) => {
+    if (keys.length === 0) return '';
+    const name = ref !== undefined ? ref!.table : keys.map(capitalizeFirstLetter).join('And');
+    let params;
+    if (ref !== undefined) {
+      this.imports.push(`import { ${capitalizeFirstLetter(ref.table)} } from '../entity/${ref.table}';`);
+      params = [`${ref.table}: Pick<${capitalizeFirstLetter(ref.table)}, '${ref.column}'>`];
+    } else {
+      params = keys.map(it => `${it}: ${columnTypeToTsType(table.columns.find(it => it.columnName)!.type)}`);
     }
-    return false;
+    const returns = unique ? `${getEntityName(table)} | undefined` : `${getEntityName(table)}[]`;
+    const findParamMap = ref !== undefined ? [`${ref.column}: ${ref.table}.${ref.column}`] : keys;
+    return createFindFunction(name, params, returns, findParamMap, unique);
   };
 
-  if (table.primaryKey) functions.push({ keys: table.primaryKey, unique: true });
-  functions.push(
-    ...table.references
-      .filter(it => !isDuplicate([it.column]))
-      .map(it => ({ keys: [it.column], unique: it.unique, ref: { table: it.table, column: it.column } })),
-  );
-  functions.push(
-    ...table.uniqueKeys.filter(it => !isDuplicate(it)).map(it => ({ keys: it, unique: true, isRef: false })),
-  );
-  return functions.map(it => findBy(table, it.keys, it.unique, it.ref)).join('\n\n');
-};
+  private functions = (table: TableInfo) => {
+    const functions: Array<{
+      keys: string[];
+      unique: boolean;
+      ref?: Pick<ReferenceColumnInfo, 'table' | 'column'>;
+    }> = [];
+    const isDuplicate = (keys: string[]) => {
+      outer: for (const fn of functions) {
+        if (keys.length !== fn.keys.length) continue;
+        for (const [i, it] of keys.entries()) {
+          if (it !== fn.keys[i]) continue outer;
+        }
+        return true;
+      }
+      return false;
+    };
 
-const createRepositoryString = (table: TableInfo) => {
-  const entity = getEntityName(table);
-  const pKeys = table.primaryKey ? `\n    ${table.primaryKey.map(it => `'${it}'`).join('\n    ')}` : '';
-  return `${importStatement(table)}
+    if (table.primaryKey) functions.push({ keys: table.primaryKey, unique: true });
+    functions.push(
+      ...table.references
+        .filter(it => !isDuplicate([it.column]))
+        .map(it => ({ keys: [it.column], unique: it.unique, ref: { table: it.table, column: it.column } })),
+    );
+    functions.push(
+      ...table.uniqueKeys.filter(it => !isDuplicate(it)).map(it => ({ keys: it, unique: true, isRef: false })),
+    );
+    return functions.map(it => this.createFindBy(table, it.keys, it.unique, it.ref)).join('\n');
+  };
+  createRepositoryString = () => {
+    const table = this.table;
+    const entity = getEntityName(table);
+    const pKeys = table.primaryKey ? `\n    ${table.primaryKey.map(it => `'${it}'`).join('\n    ')}` : '';
+    const functions = this.functions(table);
+    return `\
+${this.imports.join('\n')}
 
 export class ${entity}Repository extends SasatRepository<${entity}, Creatable${entity}> {
   protected tableName = '${table.tableName}';
   protected primaryKeys: string[] = [${pKeys}
   ];
 
-${functions(table)}
-}
+${functions}}
 `;
-};
+  };
+}
 
 export const writeRepositoryFiles = async (tables: TableInfo[]) => {
   const outDir = path.join(config().migration.out, 'repository');
   mkDirIfNotExists(outDir);
   await emptyDir(outDir);
   return await Promise.all(
-    tables.map(table => writeFile(path.join(outDir, table.tableName + '.ts'), createRepositoryString(table))),
+    tables.map(table =>
+      writeFile(path.join(outDir, table.tableName + '.ts'), new RepositoryGenerator(table).createRepositoryString()),
+    ),
   );
 };
