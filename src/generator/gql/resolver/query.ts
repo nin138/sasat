e3d42075a5d6922ad57cd88745cf666a793ae107
yanokunpei <ninian138@gmail.com>
2019-12-12 13:13:14 +0900

    impl gql get list

--- src/generator/gql/resolver/query.ts
+++ src/generator/gql/resolver/query.ts
@@ -1,20 +1,24 @@
 import { TableGenerator } from '../../store';
 import { getGqlQueries } from '../gql';
 import { toRepoFnName } from '../../repository/repository';
+import { plural } from '../../../util/stringUtil';
 
 const generateQueryStrings = (tables: TableGenerator[]) => {
   return tables
     .map(table => {
       const queries = getGqlQueries(table);
-      return queries.map(query => {
-        const params = query.params.map(it => it.name).join(', ');
-        return `${query.name}: (_: {}, { ${params} }: Pick<${table.entityName()}, ${query.params
-          .map(it => `'${it.name}'`)
-          .join(' | ')}>) => new ${table.entityName()}Repository().findBy${toRepoFnName(
-          query.params.map(it => it.name),
-          table,
-        )}(${params}),`;
-      });
+      return [
+        `${plural(table.tableName)}: () => new ${table.entityName()}Repository().list(),`,
+        ...queries.map(query => {
+          const params = query.params.map(it => it.name).join(', ');
+          return `${query.name}: (_: {}, { ${params} }: Pick<${table.entityName()}, ${query.params
+            .map(it => `'${it.name}'`)
+            .join(' | ')}>) => new ${table.entityName()}Repository().findBy${toRepoFnName(
+            query.params.map(it => it.name),
+            table,
+          )}(${params}),`;
+        }),
+      ];
     })
     .flat();
 };
