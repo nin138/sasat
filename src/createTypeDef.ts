2de2885759c787605e71390c4598ac16b75130d2
yanokunpei <ninian138@gmail.com>
2020-01-18 05:27:34 +0900

    add test createTypeDef

--- src/createTypeDef.ts
+++ src/createTypeDef.ts
@@ -1,6 +1,3 @@
-import { IrGqlType } from './ir/gql/types';
-import { getGqlTypeString } from './generator/gql/typeDef';
-
 const createTypeString = (key: string, values: string[]) => `\
 type ${key} {
 ${values.map(it => `  ${it}`).join('\n')}
---
+++ test/createTypeDef.test.ts
@@ -0,0 +1,17 @@
+import { createTypeDef } from '../src';
+
+describe('createTypeDef', () => {
+  it('should ', function() {
+    expect(createTypeDef({ T1: ['q1', 'q2'], T2: ['q3', 'q4'] })).toBe(`\
+type T1 {
+  q1
+  q2
+}
+
+type T2 {
+  q3
+  q4
+}
+`);
+  });
+});
