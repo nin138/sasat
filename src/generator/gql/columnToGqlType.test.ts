aa7d93287b0a193f40e0829ebb712706c6b7b6e0
yanokunpei <ninian138@gmail.com>
2020-07-07 14:40:13 +0900

    add test columnToGqlPrimitive

---
+++ src/generator/gql/columnToGqlType.test.ts
@@ -0,0 +1,24 @@
+import { columnTypeToGqlPrimitive } from './columnToGqlType';
+import { DBColumnTypes } from '../../migration/column/columnTypes';
+import { GqlPrimitive } from './types';
+
+describe('columnToGqlPrimitive', () => {
+  it('int', () => {
+    expect(columnTypeToGqlPrimitive(DBColumnTypes.int)).toBe(GqlPrimitive.Int);
+  });
+  it('string', () => {
+    expect(columnTypeToGqlPrimitive(DBColumnTypes.varchar)).toBe(
+      GqlPrimitive.String,
+    );
+  });
+  it('boolean', () => {
+    expect(columnTypeToGqlPrimitive(DBColumnTypes.boolean)).toBe(
+      GqlPrimitive.Boolean,
+    );
+  });
+  it('float', () => {
+    expect(columnTypeToGqlPrimitive(DBColumnTypes.double)).toBe(
+      GqlPrimitive.Float,
+    );
+  });
+});
