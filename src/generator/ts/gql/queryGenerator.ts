ed26380e227060ad0cc0acb0b6d25be00c4a2b99
n <ninian138@gmail.com>
2022-04-18 00:35:41 +0900

    update query type generation

--- src/generator/ts/gql/queryGenerator.ts
+++ src/generator/ts/gql/queryGenerator.ts
@@ -13,10 +13,10 @@ export class QueryGenerator {
           'const',
           tsg.identifier('query'),
           tsg.object(...nodes.flatMap(node => this.entity(node))),
-          tsg.typeRef('IResolverObject').importFrom('graphql-tools'),
+          tsg.typeRef('IResolvers').importFrom('@graphql-tools/utils/Interfaces'),
         )
         .export(),
-    );
+    ).addImport(['GraphQLResolveInfo'], 'graphql');
   }
   private entity(node: RepositoryNode): PropertyAssignment[] {
     const createParams = (query: QueryNode) => {
@@ -25,7 +25,7 @@ export class QueryGenerator {
           tsg.parameter('_1', tsg.typeRef('unknown')),
           tsg.parameter('_2', tsg.typeRef('unknown')),
           tsg.parameter('_3', tsg.typeRef('unknown')),
-          tsg.parameter('info'),
+          tsg.parameter('info', tsg.typeRef('GraphQLResolveInfo')),
         ];
       const paramNames = query.queryParams.map(it => it.name);
       return [
@@ -35,7 +35,7 @@ export class QueryGenerator {
           node.entityName.getTypeReference(Directory.paths.generated).pick(...paramNames),
         ),
         tsg.parameter('_2', tsg.typeRef('unknown')),
-        tsg.parameter('info'),
+        tsg.parameter('info', tsg.typeRef('GraphQLResolveInfo')),
       ];
     };
     return node.queries.map(it =>
