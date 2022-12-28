646c049771585539cde1d1d732d11535530abfae
nin138 <ninian138@gmail.com>
2022-12-28 16:13:31 +0900

    rm mariadb & update dependency

--- test/testServer.ts
+++ test/testServer.ts
@@ -1,14 +1,16 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from '@apollo/server';
 import { resolvers } from './out/__generated__/resolver.js';
 import { inputs, typeDefs } from './out/__generated__/typeDefs.js';
-import { createTypeDef } from '../src/runtime/createTypeDef.js';
+import { createTypeDef } from '../src/index.js';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const server = new ApolloServer({
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Context = {};
+
+const server = new ApolloServer<Context>({
   typeDefs: createTypeDef(typeDefs, inputs),
   resolvers,
-  debug: true,
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+const { url } = await startStandaloneServer(server);
+console.log(`🚀 Server ready at ${url}`);
