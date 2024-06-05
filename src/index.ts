612d82deefd228dfd7c51d79da90a60d29136825
nin138 <ninian138@gmail.com>
2024-06-06 00:32:42 +0900

    change build system

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { ListQueryOption } from './runtime/sasatDBDatasource.js';
-export {
-  ResolverMiddleware,
-  makeParamsMiddleware,
-} from './runtime/resolverMiddleware.js';
+export { Sql } from 'runtime/dsl/query/sql/nodeToSql.js';
+export { SqlString } from 'runtime/sql/sqlString.js';
+export type { ResolverMiddleware } from './runtime/resolverMiddleware.js';
+export { makeParamsMiddleware } from './runtime/resolverMiddleware.js';
 export { makeNumberIdEncoder } from './runtime/id.js';
 export type { CustomCondition } from './runtime/types.js';
 export { Queries } from './migration/makeQuery.js';
@@ -28,13 +28,14 @@ export type {
 export { QExpr } from './runtime/dsl/factory.js';
 export { QExpr as qe } from './runtime/dsl/factory.js';
 export { gqlResolveInfoToField } from './runtime/gqlResolveInfoToField.js';
-export {
-  SasatDBDatasource,
+
+export type {
   EntityResult,
   EntityType,
   ListQueryOption,
   QueryOptions,
 } from './runtime/sasatDBDatasource.js';
+export { SasatDBDatasource } from './runtime/sasatDBDatasource.js';
 export { getCurrentDateTimeString } from './util/dateUtil.js';
 export { getDbClient } from './db/getDbClient.js';
 export { assignDeep } from './util/assignDeep.js';
@@ -54,3 +55,5 @@ export { makeResolver } from './runtime/makeResolver.js';
 export type PagingOption = ListQueryOption; // TODO
 // export {PagingOption} from "./runtime/sql/runQuery.js";
 export { pagingOption } from './runtime/pagingOption.js';
+
+export type { SQLTransaction } from './db/connectors/dbClient';
